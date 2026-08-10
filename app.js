b219bbc6cf0e214db3033340b45ad6ddf7195ab0
DMHacker123 <matosalvarezdm@hotmail.com>
2026-08-10 18:49:27 -0400

    Add user CRUD and improve recipe routes

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
 const recipeRoutes = require('./routes/recipes');
 const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/recipes', recipeRoutes);
 
 app.get('/', (req, res) => {
   res.send('Recipe Sharing API is running');
 });
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/recipes', recipeRoutes);
 
 module.exports = app;
